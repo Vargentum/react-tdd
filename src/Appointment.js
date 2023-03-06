05416694d70f8c7d9c236a4a53564076f6e542d2
Vladislav Serebriakov <vladislav.serebriakov@x2sy.com>
2023-03-06 11:27:38 +0200

    chapter 2 before exercises

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,5 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+const appointmentTimeOfDay = (startsAt) => {
+  const [h, m] = new Date(startsAt)
+    .toTimeString()
+    .split(":");
+  return `${h}:${m}`;
+}
 
 export const Appointment = ({customer}) => (
-  <div>{customer.firstName}</div>
+  <div>
+    {customer.firstName}
+  </div>
 )
+
+export const AppointmentDayView = ({appointments}) => {
+  const [selectedAppointmentIndex, setSelectedAppointmentIndex] = useState(0);
+
+  return (
+    <div id="appointmentsDayView">
+      {appointments.length > 0 
+        ? <Appointment {...appointments[selectedAppointmentIndex]} />
+        : <p>There are no appointments scheduled for today.</p>
+      }
+      <ol>
+        {appointments.map((appointment, appointmentIndex) => 
+          <li key={appointment.startAt}>
+            <button 
+              type="button" 
+              onClick={() => setSelectedAppointmentIndex(appointmentIndex)}
+            >
+              {appointmentTimeOfDay(appointment.startAt)}
+            </button>
+          </li>
+        )}
+      </ol>
+    </div>
+  )
+}
